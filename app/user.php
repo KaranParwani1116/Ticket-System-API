c7e93ebb02bc07e15d792f29c8f0de3cddaa2c86
KaranParwani1116 <karanparwani.parwani102@gmail.com>
2020-08-29 00:22:05 +0530

    Added error middleware

---
+++ app/user.php
@@ -0,0 +1,5 @@
+<?php
+$app->post('/booktickets', function($request, $response, $args) {
+    
+});
+?>
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,18 +14,32 @@ $app->setBasePath('/ticketsystem/public');
 
 $app->addRoutingMiddleware();
 
+$customErrorHandler = function (
+    ServerRequestInterface $request,
+    Throwable $exception,
+    bool $displayErrorDetails,
+    bool $logErrors,
+    bool $logErrorDetails,
+    ?LoggerInterface $logger = null
+) use ($app) {
+    $payload = array();
+    $payload['status'] = $exception->getCode();
+    $payload['message'] = $exception->getMessage();
+
+    $response = $app->getResponseFactory()->createResponse();
+    $response->getBody()->write( json_encode($payload));
+
+    return $response->withHeader('Content-Type', 'application/json')
+                    ->withStatus($exception->getCode() != 0 ? $exception->getCode() : 500);
+};
+
 $errorMiddleware = $app->addErrorMiddleware(true, true, true);
+$errorMiddleware->setDefaultErrorHandler($customErrorHandler);
 
 $app->add(new BeforeMiddleware());
-$app->add(new AfterMiddleware());
+//$app->add(new AfterMiddleware());
 
+require_once __DIR__ . '/../app/user.php';
 
-$app->get('/hello/{name}', function ($request, $response, $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-// Run app
 $app->run();
 
