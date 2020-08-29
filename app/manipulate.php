8b28beca16fd1446cc072cf42bc8ed39ef40fd9c
KaranParwani1116 <karanparwani.parwani102@gmail.com>
2020-08-29 19:08:23 +0530

    route for update ticket timings created

--- app/manipulate.php
+++ app/manipulate.php
@@ -1,3 +1,29 @@
 <?php
 
+//route to update ticket timings
+$app->post('/updatetiming', function($request, $response, $args) {
+  require_once __DIR__. '/../bootstrap/dbconnect.php';
+
+  $requestData = array();
+
+  $requestData['t_id'] = $request->getParsedBody()['t_id'];
+  $requestData['timing'] = $request->getParsedBody()['timing'];
+
+  $query = $pdo->prepare("UPDATE `tickets` SET `timing` = :timing WHERE `t_id` = :t_id");
+  $query->execute($requestData);
+
+  $errorData = $query->errorInfo();
+
+  if($errorData[1]) {
+      return checkError($response, $errorData);
+  }
+
+  $output['status'] = 200;
+  $output['message'] = "Tickets timing updated successfully";
+
+  $payload = json_encode($output);
+  $response->getBody()->write($payload);
+
+  return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+})
 ?>
